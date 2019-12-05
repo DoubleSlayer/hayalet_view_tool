f5046ab7a6f32fa79e2e2e4b0d42800728990826
Sinan KILIÇ <sinankilic61@gmail.com>
2019-12-05 13:55:08 +0300

    Dev tool initial commit

---
+++ lib/hayalet_view_tool.rb
@@ -0,0 +1,7 @@
+require "hayalet_view_tool/version"
+require "hayalet_view_tool/renderer"
+
+module HayaletViewTool
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/hayalet_view_tool/version.rb
@@ -0,0 +1,3 @@
+module HayaletViewTool
+  VERSION = "0.1.0"
+end
